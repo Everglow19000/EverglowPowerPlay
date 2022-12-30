11f10f2afe7e2ec05a863b6f2f6bf34c3b9c152a
guyKN <guyknaan@gmail.com>
2022-12-30 22:00:07 +0200

    Add option for unit tests.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RunTests.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import org.junit.Test;
+
+public class RunTests {
+    @Test
+    public void test(){
+        System.out.println("Test running");
+    }
+}
