8aec897fd36b42354df52d82f4fb56c8788798d4
guyKN <guyknaan@gmail.com>
2022-12-30 23:47:42 +0200

    Fix bug in odeSolver

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RunTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RunTests.java
@@ -1,10 +1,29 @@
 package org.firstinspires.ftc.teamcode.test;
 
+import org.firstinspires.ftc.teamcode.utils.OdeSolver;
 import org.junit.Test;
+
+import java.util.List;
 
 public class RunTests {
     @Test
     public void test(){
         System.out.println("Test running");
+    }
+
+    @Test
+    public void testOdeSolver(){
+
+        List<Double> result = OdeSolver.rungeKutta45(
+                (x)->x,
+                0.01,
+                1,
+                100
+        );
+
+        for(double d: result){
+            System.out.println(d);
+        }
+
     }
 }
