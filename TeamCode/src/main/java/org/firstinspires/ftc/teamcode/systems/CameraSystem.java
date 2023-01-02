e9a06c5839999fd53e99e760c83a617c5c63e781
guyKN <guyknaan@gmail.com>
2023-01-02 18:44:02 +0200

    Draw outline of cone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/CameraSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/CameraSystem.java
@@ -7,7 +7,11 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.utils.AndroidUtils;
 import org.firstinspires.ftc.teamcode.utils.CameraCalibration;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.apriltag.AprilTagDetectorJNI;
@@ -18,6 +22,9 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A Class for handling the image processing for camera.
@@ -45,17 +52,39 @@ public class CameraSystem {
 		private long nativeAprilTagPtr; // A pointer to the JNI AprilTag detector
 		private final Mat grey = new Mat(); // A blank canvas to store the b&w image
 
+		// mats used for image detection
+		private Mat hsv;
+		private Mat redMask1;
+		private Mat redMask2;
+		private Mat redMask;
+
+		// thresholds for detection by color
+		private static final Scalar RED_THRESHOLD_1_LOWER = new Scalar(0, 100, 20);
+		private static final Scalar RED_THRESHOLD_1_UPPER = new Scalar(10, 255, 255);
+		private static final Scalar RED_THRESHOLD_2_LOWER = new Scalar(160, 100, 20);
+		private static final Scalar RED_THRESHOLD_2_UPPER = new Scalar(180, 255, 255);
+
 		private final LinearOpMode opMode; // The OpMode that's currently running
 		private boolean isCapturingImage = false;
 		private boolean isDetectingAprilTag = false;
 
 		public AprilTagType aprilTagID = AprilTagType.UNIDENTIFIED; // Last identified AprilTag
 
+
+
 		public CameraPipeline(LinearOpMode opMode) {
 			this.opMode = opMode;
 
 			// initiate AprilTag detector
 			nativeAprilTagPtr = AprilTagDetectorJNI.createApriltagDetector(AprilTagDetectorJNI.TagFamily.TAG_36h11.string, 3, 3);
+		}
+
+		@Override
+		public void init(Mat mat) {
+			hsv = Mat.zeros(mat.size(), mat.type());
+			redMask1 = Mat.zeros(mat.size(), CvType.CV_8U);
+			redMask2 = Mat.zeros(mat.size(), CvType.CV_8U);
+			redMask = Mat.zeros(mat.size(), CvType.CV_8U);
 		}
 
 		// runs when class is deleted
@@ -144,9 +173,44 @@ public class CameraSystem {
 					throw new RuntimeException(e);
 				}
 			}
+
+
+			detectCone(input);
 			return input;
 		}
+
+		private static MatOfPoint largestContour(List<MatOfPoint> contours){
+			double largestContourArea = 0;
+			MatOfPoint largestContour = null;
+			for (MatOfPoint contour: contours){
+				double area = Imgproc.contourArea(contour);
+				if (area > largestContourArea){
+					largestContourArea = area;
+					largestContour = contour;
+				}
+			}
+			return largestContour;
+		}
+
+
+		public void detectCone(Mat input){
+			Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
+			Core.inRange(hsv, RED_THRESHOLD_1_LOWER, RED_THRESHOLD_1_UPPER, redMask1);
+			Core.inRange(hsv, RED_THRESHOLD_2_LOWER, RED_THRESHOLD_2_UPPER, redMask2);
+			Core.bitwise_or(redMask1, redMask2, redMask);
+
+			List<MatOfPoint> contours = new ArrayList<>();
+			Mat hierarchy = new Mat();
+
+			Imgproc.findContours(redMask, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+			MatOfPoint largestContour = largestContour(contours);
+
+			Imgproc.drawContours(input, Collections.singletonList(largestContour), 0, new Scalar(255, 255, 255), 20);
+		}
+
 	}
+
 
 	/**
 	 * @param opMode The current opMode running on the robot.
