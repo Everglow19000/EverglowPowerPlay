8f64bcab2eaa085e98e8eccc8562ea83df196e1f
guyKN <guyknaan@gmail.com>
2023-02-02 12:02:47 +0200

    Add cameraTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/CameraTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.systems.CameraSystem;
+import org.firstinspires.ftc.teamcode.systems.DrivingSystem;
+import org.firstinspires.ftc.teamcode.utils.EverglowGamepad;
+import org.firstinspires.ftc.teamcode.utils.Pose;
+
+@TeleOp(name = "CameraTest", group = "Test")
+public class CameraTest extends LinearOpMode {
+	@Override
+	public void runOpMode() {
+		EverglowGamepad gamepad = new EverglowGamepad(gamepad1);
+		DrivingSystem drivingSystem = new DrivingSystem(this);
+		CameraSystem cameraSystem = new CameraSystem(this);
+		Pose actPowers = new Pose();
+
+		waitForStart();
+
+		cameraSystem.captureImage();
+
+		while (opModeIsActive()) {
+			gamepad.update();
+
+			// Assign image capture to the circle button
+			if (gamepad.circle()) {
+				cameraSystem.captureImage();
+			}
+
+			// Assign AprilTag detection to cross button
+			if (gamepad.cross()) {
+				telemetry.addLine("Detecting AprilTag...");
+
+				CameraSystem.AprilTagType aprilTagType = cameraSystem.detectAprilTag();
+				telemetry.addData("AprilTag ID:", aprilTagType);
+				telemetry.update();
+			}
+
+			actPowers.x = -gamepad1.left_stick_x;
+			actPowers.y = -gamepad1.left_stick_y;
+			actPowers.angle = -gamepad1.right_stick_x;
+			drivingSystem.driveMecanum(actPowers);
+		}
+	}
+}
+
